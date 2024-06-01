d31fa685d485cf04e4439a06908bb6940670c8fc
Dey-Jess <ortegadeivids4@gmail.com>
2024-06-01 10:12:25 -0500

    Se cambio la vista del item del producto y la funcionalidad para pasar otra vez a ala vista del home

--- app/src/main/java/com/example/origenes/vistaProducto.java
+++ app/src/main/java/com/example/origenes/vistaProducto.java
@@ -3,6 +3,9 @@ package com.example.origenes;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -24,6 +27,7 @@ public class vistaProducto extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_vista_producto);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
 
         if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
@@ -51,6 +55,21 @@ public class vistaProducto extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Find the button by its ID
+        ImageView loginButton = findViewById(R.id.back);
+
+        // Set a click listener on the button
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create an intent to navigate to the LoginActivity
+                Intent intent = new Intent(vistaProducto.this , HomeActivity.class);
+
+                // Start the LoginActivity
+                startActivity(intent);
+            }
         });
     }
 }
